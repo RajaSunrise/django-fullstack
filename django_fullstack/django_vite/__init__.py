ffb6e6f3f8069f026af1e6f1972287f970dcb722
RajaSunrise <rajasunsrise@gmail.com>
2025-07-07 19:03:39 +0800

    feat: change uv

--- django_fullstack/django_vite/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core.asset_loader import DjangoViteConfig
-
-__all__ = ["DjangoViteConfig"]
--- django_fullstack/inertia/__init__.py
+++
@@ -1,3 +0,0 @@
-from .http import inertia, render
-from .utils import lazy
-from .share import share
